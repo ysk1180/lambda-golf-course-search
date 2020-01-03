70e4d37a2fa4d3e748c4d57a4df9caa20f13e6ee
Yusuke Miyazaki <ysk1180@gmail.com>
2020-01-03 13:44:31 +0900

    courseをplanにリネーム

--- search.rb
+++ search.rb
@@ -25,34 +25,37 @@ def lambda_handler(event:, context:)
     c.affiliate_id = ENV['RAKUTEN_AFID']
   end
 
-  matched_courses = []
+  matched_plans = []
   1.upto(2) do |page| # API Gatewayが30秒でタイムアウトするからそれのギリギリの2ページまでにしてる(1ページ30件)
-    courses = RakutenWebService::Gora::Plan.search(page: page, maxPrice: budget, playDate: date, areaCode: '8,11,12,13,14', NGPlan: 'planHalfRound,planLesson,planOpenCompe,planRegularCompe')
+    plans = RakutenWebService::Gora::Plan.search(page: page, maxPrice: budget, playDate: date, areaCode: '8,11,12,13,14', NGPlan: 'planHalfRound,planLesson,planOpenCompe,planRegularCompe')
 
-    courses.each do |course|
-      next if course['golfCourseName'] =~ /.*(レッスン|ショート|7ホール|ナイター).*/ # 不要そうな文字が入ってたらスキップ
-      course_duration = Duration.find(golf_course_id: course['golfCourseId']).send("duration#{departure}")
-      next if course_duration > duration # 希望の所要時間より長いものの場合はスキップ
-      matched_courses.push(
+    plans.each do |plan|
+      next if plan['golfCourseName'] =~ /.*(レッスン|ショート|7ホール|ナイター).*/ # 不要そうな文字が入ってたらスキップ
+      plan_duration = Duration.find(golf_course_id: plan['golfCourseId']).send("duration#{departure}")
+      next if plan_duration > duration # 希望の所要時間より長いものの場合はスキップ
+      matched_plans.push(
         {
-          name: course['golfCourseName'],
-          caption: course['golfCourseCaption'],
-          prefecture: course['prefecture'],
-          image_url: course['golfCourseImageUrl'],
-          evaluation: course['evaluation'],
-          plan_name: course['planInfo'][0]['planName'],
-          price: course['planInfo'][0]['price'],
-          duration: course_duration,
-          reserve_url_pc: course['planInfo'][0]['callInfo']['reservePageUrlPC'],
-          reserve_url_mobile: course['planInfo'][0]['callInfo']['reservePageUrlMobile'],
+          name: plan['golfCourseName'],
+          caption: plan['golfCourseCaption'],
+          prefecture: plan['prefecture'],
+          image_url: plan['golfCourseImageUrl'],
+          evaluation: plan['evaluation'],
+          plan_name: plan['planInfo'][0]['planName'],
+          price: plan['planInfo'][0]['price'],
+          duration: plan_duration,
+          reserve_url_pc: plan['planInfo'][0]['callInfo']['reservePageUrlPC'],
+          reserve_url_mobile: plan['planInfo'][0]['callInfo']['reservePageUrlMobile'],
         }
       )
     end
 
-    break unless courses.has_next_page?
+    break unless plans.has_next_page?
   end
 
-  matched_courses.sort_by! {|course| course[:duration]}
+  matched_plans.sort_by! {|plan| plan[:duration]}
 
-  { courses: matched_courses }
+  {
+    count: matched_plans.count,
+    plans: matched_plans
+  }
 end
